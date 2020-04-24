3717b98cbb2c73a0a04caa60349d7b33fd086322
jghori <59810056+jghori@users.noreply.github.com>
2020-04-24 15:41:26 -0400

    Updates

---
+++ precache-manifest.686b3cc74bc8524a9fb3b2e9634a53f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa9fff4c81355b850155ed6a9e6f700",
+    "url": "/the-covid-curve/index.html"
+  },
+  {
+    "revision": "38bdb8f36f3532e08498",
+    "url": "/the-covid-curve/static/css/main.b10f43b3.chunk.css"
+  },
+  {
+    "revision": "19b50cdca90d3e3172a9",
+    "url": "/the-covid-curve/static/js/2.4378980d.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/the-covid-curve/static/js/2.4378980d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38bdb8f36f3532e08498",
+    "url": "/the-covid-curve/static/js/main.f257799c.chunk.js"
+  },
+  {
+    "revision": "81e82b3c6902fd26929c",
+    "url": "/the-covid-curve/static/js/runtime-main.35e61b5c.js"
+  },
+  {
+    "revision": "8b1012c8bf50379bb33c350a46831555",
+    "url": "/the-covid-curve/static/media/image.8b1012c8.png"
+  }
+]);
